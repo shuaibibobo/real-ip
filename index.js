1c5c9a2a24fee7b40f9718aa90940fa3f3b51346
zfcsoftware <zfcsoftware@gmail.com>
2024-04-14 19:37:20 +0300

    first

---
+++ index.js
@@ -0,0 +1,52 @@
+const requestIp = require('request-ip');
+
+
+const checkIPFormat = (user_ip) => {
+    try {
+        return !user_ip || user_ip === undefined || user_ip === null || (String(user_ip).indexOf('.') <= -1 && String(user_ip).indexOf(':') <= -1)
+    } catch (err) {
+        return null
+    }
+}
+
+
+function detect({ req, config }) {
+    try {
+        var user_ip = config.cloudflare ? req.headers['cf-connecting-ip'] : null
+
+        if (checkIPFormat(user_ip)) {
+            var hdip = String(req.headers['x-forwarded-for']).split(',')
+            user_ip = hdip[hdip.length - 1]
+        }
+
+        if (checkIPFormat(user_ip)) {
+            var hdip2 = String(req.headers['x-real-ip']).split(',')
+            user_ip = hdip2[hdip2.length - 1]
+        }
+
+
+        if (checkIPFormat(user_ip)) {
+            user_ip = requestIp.getClientIp(req)
+        }
+
+
+        if (checkIPFormat(user_ip)) {
+            user_ip = null
+        }
+
+
+        if (!checkIPFormat(user_ip)) {
+            user_ip = String(user_ip)
+                .replace(/::ffff:/g, '')
+                .trim()
+        }
+        return user_ip
+        
+    } catch (err) {
+        return null
+    }
+}
+
+module.exports = {
+    detect
+}
